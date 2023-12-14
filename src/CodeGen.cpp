aba949329605cca2c98b5093c99edd4b73ac8551
Arshia Paridari <100465773+Arshiapdr@users.noreply.github.com>
2023-12-15 02:27:57 +0330

    Add files via upload

---
+++ src/CodeGen.cpp
@@ -0,0 +1,360 @@
+#include "CodeGen.h"
+#include "llvm/ADT/StringMap.h"
+#include "llvm/IR/IRBuilder.h"
+#include "llvm/IR/LLVMContext.h"
+#include "llvm/Support/raw_ostream.h"
+
+using namespace llvm;
+
+// Define a visitor class for generating LLVM IR from the AST.
+namespace
+{
+  class ToIRVisitor : public ASTVisitor
+  {
+    Module *M;// easy IR generation
+    IRBuilder<> Builder;
+    Type *VoidTy;
+    Type *Int32Ty;
+    Type *Int8PtrTy;
+    Type *Int8PtrPtrTy;
+    Constant *Int32Zero;
+    Function *MainFn;
+    Value *V; // current calculated value updated through tree traversal
+    StringMap<AllocaInst *> nameMap;// maps a variable name to the value that's returned by calc_read()
+
+  public:
+    // Constructor for the visitor class.
+    ToIRVisitor(Module *M) : M(M), Builder(M->getContext())
+    {
+      // Initialize LLVM types and constants.
+      VoidTy = Type::getVoidTy(M->getContext());
+      Int32Ty = Type::getInt32Ty(M->getContext());
+      Int8PtrTy = Type::getInt8PtrTy(M->getContext());
+      Int8PtrPtrTy = Int8PtrTy->getPointerTo();
+      Int32Zero = ConstantInt::get(Int32Ty, 0, true);
+    }
+
+    // Entry point for generating LLVM IR from the AST.
+    void run(AST *Tree)
+    {
+      // Create the main function with the appropriate function type.
+      FunctionType *MainFty = FunctionType::get(Int32Ty, {Int32Ty, Int8PtrPtrTy}, false);
+      MainFn = Function::Create(MainFty, GlobalValue::ExternalLinkage, "main", M);
+
+      // Create a basic block for the entry point of the main function.
+      BasicBlock *BB = BasicBlock::Create(M->getContext(), "entry", MainFn);
+      Builder.SetInsertPoint(BB);
+
+      // Visit the root node of the AST to generate IR.
+      // begin the AST traversal 
+      Tree->accept(*this);
+
+      // Create a return instruction at the end of the main function.
+      Builder.CreateRet(Int32Zero);
+    }
+
+    // Visit function for the GSM node in the AST.
+    virtual void visit(GSM &Node) override
+    {
+      // Iterate over the children of the GSM node and visit each child.
+      for (auto I = Node.begin(), E = Node.end(); I != E; ++I)
+      {
+        (*I)->accept(*this);
+      }
+    };
+
+    virtual void visit(Assignment &Node) override
+    {
+      // Visit the right-hand side of the assignment and get its value.
+      Node.getRight()->accept(*this);
+      Value *val = V;
+
+      // Get the name of the variable being assigned.
+      auto varName = Node.getLeft()->getVal();
+      Value *var_value = Builder.CreateLoad(Int32Ty,nameMap[Node.getLeft()])// ex)a += 2;  -> first we should the current value of a
+      switch (Node.getOperator())
+      {
+        case Assignment::Eq:
+            break;
+        case Assignment::PlEq:
+            Value *temp = Builder.CreateNSWAdd(var_value,val);
+            val = temp;
+            break;
+        case Assignment::MulEq:
+            Value *temp = Builder.CreateNSWMul(var_value,val);
+            val = temp;
+            break;
+        case Assignment::DivEq:
+            Value *temp = Builder.CreateSDiv(var_value,val);
+            val = temp;
+            break;        
+        case Assignment::ModEq:
+            Value *temp = Builder.CreateSRem(var_value,val);
+            val = temp;
+            break;
+        case Assignment::MinEq:
+            Value *temp = Builder.CreateNSWSub(var_value,val);
+            val = temp;
+            break;
+      }
+
+      // Create a store instruction to assign the value to the variable.
+      Builder.CreateStore(val, nameMap[varName]);
+
+      // Create a function type for the "gsm_write" function.
+      FunctionType *CalcWriteFnTy = FunctionType::get(VoidTy, {Int32Ty}, false);
+      // Create a function declaration for the "gsm_write" function.
+      Function *CalcWriteFn = Function::Create(CalcWriteFnTy, GlobalValue::ExternalLinkage, "gsm_write", M);
+
+      // Create a call instruction to invoke the "gsm_write" function with the value.
+      CallInst *Call = Builder.CreateCall(CalcWriteFnTy, CalcWriteFn, {val});
+    };
+
+    virtual void visit(Factor &Node) override
+    {
+      if (Node.getKind() == Factor::Ident)
+      {
+        // If the factor is an identifier, load its value from memory.
+        V = Builder.CreateLoad(Int32Ty, nameMap[Node.getVal()]);
+      }
+      else
+      {
+        // If the factor is a literal, convert it to an integer and create a constant.
+        int intval;
+        Node.getVal().getAsInteger(10, intval);
+        V = ConstantInt::get(Int32Ty, intval, true);
+      }
+    };
+
+    virtual void visit(BinaryOp &Node) override
+    {
+      // Visit the left-hand side of the binary operation and get its value.
+      Node.getLeft()->accept(*this);
+      Value *Left = V;
+
+      // Visit the right-hand side of the binary operation and get its value.
+      Node.getRight()->accept(*this);
+      Value *Right = V;
+
+      // Perform the binary operation based on the operator type and create the corresponding instruction.
+      switch (Node.getOperator())
+      {
+      case BinaryOp::Or:
+        V = Builder.CreateOr(Left, Right);
+        break;
+      case BinaryOp::And:
+        V = Builder.CreateAnd(Left, Right);
+        break;
+      case BinaryOp::IsEq:
+        V = Builder.CreateICmpEQ(Left, Right);
+        break;
+      case BinaryOp::IsNEq:
+        V = Builder.CreateICmpNE(Left, Right);
+        break;
+      case BinaryOp::GrEq:
+        V = Builder.CreateICmpSGE(Left, Right);
+        break;
+      case BinaryOp::LoEq:
+        V = Builder.CreateICmpSLE(Left, Right);
+        break;
+      case BinaryOp::Gr:
+        V = Builder.CreateICmpSGT(Left, Right);
+        break;
+      case BinaryOp::Lo:
+        V = Builder.CreateICmpSLT(Left, Right);
+        break;
+      case BinaryOp::Plus:
+        V = Builder.CreateNSWAdd(Left, Right);
+        break;
+      case BinaryOp::Minus:
+        V = Builder.CreateNSWSub(Left, Right);
+        break;
+      case BinaryOp::Mul:
+        V = Builder.CreateNSWMul(Left, Right);
+        break;
+      case BinaryOp::Div:
+        V = Builder.CreateSDiv(Left, Right);
+        break;
+      case BinaryOp::Mod:
+        V = Builder.CreateSRem(Left, Right);
+        break;
+      case BinaryOp::Pow:
+        int right_value = 0;
+        Value *NewRight = (Value *) Node.getRight();
+        NewRight -> getVal().getAsInteger(10, right_value);
+        Value *NewLeft = Left;
+      
+        if (right_value == 0) {
+          V = ConstantInt::get(Int32Ty, 1, true);
+          break;
+        }
+
+        else {
+          for (int i = 0; i < right_value - 1; i++)
+          {
+           Left = Builder.CreateNSWMul(Left, NewLeft);
+          }
+
+          V = Left;  
+          break;
+      }
+    };
+
+    virtual void visit(Declaration &Node) override
+    {
+      auto Exprs_iterator = Node.beginExprs();
+      auto Vars_iterator = Node.beginVars();
+      //by the end of this loop we have assigned each declared variable with corresponding expression value
+      for(Exprs_iterator;Exprs_iterator != Node.endExprs();++Exprs_iterator,++Vars_iterator)
+      {
+            (*Exprs_iterator)->accept(*this);
+            Value *val = V; //V will get assined with the final value of expression which could be assignment-BinaryOpration etc..
+            StringRef Var = *Vars_iterator;
+            nameMap[Var] = Builder.CreateAlloca(Int32Ty);
+            Builder.CreateStore(val,nameMap[Var]);
+      }
+      // instanciate remaining declared variables with 0
+      for(Vars_iterator;Vars_iterator != Node.endVars(),Vars_iterator++)
+      {
+            Value *zero = ConstantInt::get(Int32Ty,0,true);
+            StringRef Var = *Vars_iterator;
+            nameMap[Var] = Builder.CreateAlloca(Int32Ty);
+            Builder.CreateStore(zero,nameMap[Var]); // I think insted of zero we could use 'Int32Zero'
+      } 
+    };
+    
+    virtual void visit(IfElse &Node) override
+    {
+      // Create basic blocks for if, elif, else, and merge
+      
+      BasicBlock *MergeBB = BasicBlock::Create(M->getContext(), "merge", MainFn);
+      BasicBlock *ElseBB = nullptr;
+
+      if(Node.getHasElse())//if we have an else statment we create its BB
+      {
+        ElseBB = BasicBlock::Create(M->getContext(), "else", MainFn); 
+      }
+      // Iterate through each expression and corresponding assignments
+      auto exprIterator = Node.beginExprs();
+      auto assignIterator = Node.beginAssigns2D();
+
+
+  
+      for (; exprIterator != Node.endExprs(); ++exprIterator, ++assignIterator)
+      {
+        BasicBlock *IfBB = BasicBlock::Create(M->getContext(), "if", MainFn); 
+        Builder.Create(IfBB);//check if condition
+        setCurr(IfBB);
+        // Emit LLVM IR instructions for the condition
+        (*exprIterator)->accept(*this);
+        Value *Condition = V;
+        
+        Builder.CreateCondBr(Condition,)  
+        //conditional branch to Merge considering the presence of an else statement 
+        Builder.CreateCondBr(Condition, IfBB, ElseBB ? ElseBB : MergeBB);
+
+        setCurr(IfBB);
+        auto IfAssignments = *assignIterator; //first row of 2D vector
+        for(auto a : IfAssignments.begin; a != IfAssignments.end();++a)// a is represents each assignment in the first row
+        {
+          a->accept(*this);// do each assignment in the if statement   
+        }
+        // goto merge BB because the whole ifElse node is performed
+        Builder.CreateBr(MergeBB);
+      }
+
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+    
+      // First expression corresponds to 'if', subsequent expressions to 'elif'
+      for (; exprIterator != Node.endExprs(); ++exprIterator, ++assignIterator)
+      {
+          BasicBlock *IfBB = BasicBlock::Create(M->getContext(), "if", MainFn);
+
+          // Emit LLVM IR instructions for the condition
+          (*exprIterator)->accept(*this);
+          Value *Condition = V;
+
+          // Create conditional branch instructions
+          Builder.CreateCondBr(Condition, IfBB, ElseBB ? ElseBB : MergeBB);
+
+          // Emit LLVM IR for the 'if' block
+          setCurr(IfBB);
+          for (auto assign : *assignIterator) {
+              assign->accept(*this);
+          }
+          
+          Builder.CreateBr(MergeBB);//current = IfBB 
+
+          // If there's an 'else' block, create a new block for the next 'elif'
+          if (!ElseBB)
+              ElseBB = BasicBlock::Create(M->getContext(), "else", MainFn);
+      }
+
+      // Emit LLVM IR for the 'else' block if it exists
+      if (ElseBB)
+      {
+          setCurr(ElseBB);
+          auto elseAssignIterator = Node.elseAssigns_begin();
+          auto elseAssignEnd = Node.elseAssigns_end();
+          for (; elseAssignIterator != elseAssignEnd; ++elseAssignIterator) {
+              (*elseAssignIterator)->accept(*this);
+          }
+          Builder.CreateBr(MergeBB);
+      }
+
+      // Set the current block to the merge block
+      setCurr(MergeBB);
+    };
+
+    virtual void visit(Loop &Node) override
+    {  
+      BasicBlock *LoopCondBB = BasicBlock::Create(M->getContext(), "loop.cond", MainFn);
+      BasicBlock *LoopBodyBB = BasicBlock::Create(M->getContext(), "loop.body", MainFn);
+      BasicBlock *AfterLoopBB = BasicBlock::Create(M->getContext(), "after.loop", MainFn);
+      
+      // Emit LLVM IR instructions
+      Builder.CreateBr(LoopCondBB);
+      setCurr(LoopCondBB);
+      // Assuming there's a function to emit the loop condition expression
+      Node.getCondition()->accept(*this);
+      Value *Condition = V;
+      Builder.CreateCondBr(Condition, LoopBodyBB, AfterLoopBB);
+      setCurr(LoopBodyBB);
+      // accept statements within the loop body
+      
+      auto assignment_iterator = Node.begin()//  Node.begin() retrieves loop assignments/statements
+      for(assignment_iterator;assignment_iterator != Node.end();++assignment_iterator)
+      {
+        (*assignment_iterator)->accept(*this);
+      }
+      Builder.CreateBr(LoopCondBB);//current = LoopbodyBB -> we want to branch to LoopCondBB
+      setCurr(AfterLoopBB);
+      };
+  };
+}; // namespace
+
+
+void CodeGen::compile(AST *Tree)
+{
+  // Create an LLVM context and a module.
+  LLVMContext Ctx;
+  Module *M = new Module("calc.expr", Ctx);
+
+  // Create an instance of the ToIRVisitor and run it on the AST to generate LLVM IR.
+  ToIRVisitor ToIR(M);
+  ToIR.run(Tree);
+
+  // Print the generated module to the standard output.
+  M->print(outs(), nullptr);
+}
---
+++ src/Parser.cpp
@@ -0,0 +1,549 @@
+#include "Parser.h"
+
+// main point is that the whole input has been consumed
+AST *Parser::parse()
+{
+    AST *Res = parseGSM();
+    return Res;
+}
+
+AST *Parser::parseGSM()
+{
+    llvm::SmallVector<Expr *> exprs;
+    while (!Tok.is(Token::eoi))
+    {
+        switch (Tok.getKind())
+        {
+        case Token::KW_int:
+            Expr *declaration;
+            declaration = parseDeclaration();
+
+            if (!Tok.is(Token::semicolon))
+            {
+                error();
+                goto _error1;
+            }
+            if (declaration)
+                exprs.push_back(declaration);
+            else
+                goto _error1;
+            break;
+
+        case Token::ident:
+            Expr *assign;
+            assign = parseAssign();
+
+            if (!Tok.is(Token::semicolon))
+            {
+                error();
+                goto _error1;
+            }
+            if (assign)
+                exprs.push_back(assign);
+            else
+                goto _error1;
+            break;
+
+        case Token::KW_if:
+            Expr *ifelse;
+            ifelse = parseIfElse();
+
+            if (ifelse)
+                exprs.push_back(ifelse);
+            else
+                goto _error1;
+            break;
+
+        case Token::KW_loopc:
+            Expr *loop;
+            loop = parseLoop();
+
+            if (loop)
+                exprs.push_back(loop);
+            else
+                goto _error1;
+            break;
+
+        default:
+            goto _error1;
+            break;
+        }
+        advance(); // TODO: watch this part
+    }
+    return new GSM(exprs);
+_error1:
+    while (Tok.getKind() != Token::eoi)
+        advance();
+    return nullptr;
+}
+
+Expr *Parser::parseDeclaration()
+{
+    Expr *E;
+    int vars_count = 0;
+    int exprs_count = 0;
+    llvm::SmallVector<llvm::StringRef, 8> Vars;
+    llvm::SmallVector<Expr *> Exprs;
+
+    if (!Tok.is(Token::KW_int))
+        goto _error2;
+
+    advance();
+
+    if (expect(Token::ident))
+        goto _error2;
+
+    Vars.push_back(Tok.getText());
+    vars_count += 1;
+    advance();
+
+    while (Tok.is(Token::comma))
+    {
+        advance();
+
+        if (expect(Token::ident))
+            goto _error2;
+
+        Vars.push_back(Tok.getText());
+        vars_count += 1;
+        advance();
+    }
+
+    if (Tok.is(Token::equal))
+    {
+        advance();
+        E = parseExpression();
+        if (E) {
+            Exprs.push_back(E);
+            exprs_count += 1;
+        }
+        else {
+            goto _error2;
+        }
+
+        while (Tok.is(Token::comma))
+        {
+            advance();
+            E = parseExpression();
+            if (E) {
+                Exprs.push_back(E);
+                exprs_count += 1;
+            } 
+            else {
+                goto _error2;
+            }
+        }
+    }
+
+    if (expect(Token::semicolon) || exprs_count > vars_count)
+        goto _error2;
+
+    return new Declaration(Vars, Exprs);
+_error2: // TODO: Check this later in case of error :)
+    while (Tok.getKind() != Token::eoi)
+        advance();
+    return nullptr;
+}
+
+Expr *Parser::parseAssign()
+{
+    Factor *F;
+    Expr *E;
+    F = (Factor *)(parseFactor());
+
+    if (!Tok.is(Token::equal) || !Tok.is(Token::plus_equal) || !Tok.is(Token::mult_equal)
+    || !Tok.is(Token::div_equal) || !Tok.is(Token::minus_equal) || !Tok.is(Token::mod_equal))
+    {
+        error();
+        return nullptr;
+    }
+
+    if (Tok.is(Token::equal)) {
+        Assignment::Operator Op = Assignment::Eq;
+    }
+    else if(Tok.is(Token::plus_equal)) {
+        Assignment::Operator Op = Assignment::PlEq;
+    }
+    else if(Tok.is(Token::mult_equal)) {
+        Assignment::Operator Op = Assignment::MulEq;
+    }
+    else if(Tok.is(Token::div_equal)) {
+        Assignment::Operator Op = Assignment::DivEq;
+    }
+    else if(Tok.is(Token::minus_equal)) {
+        Assignment::Operator Op = Assignment::MinEq;
+    }
+    else {
+        Assignment::Operator Op = Assignment::ModEq;
+    }
+
+    advance();
+    E = parseExpr();
+    return new Assignment(Op, F, E);
+}
+
+Expr *Parser::parseIfElse()
+{
+    Expr *E;
+    Assignment *A;
+    llvm::SmallVector<Expr *> expressions;
+    llvm::SmallVector<llvm::SmallVector<Assignment *>> assignments;
+    llvm::SmallVector<Assignment *> temp_assignments;
+    bool hasElse = false; //NEW
+
+    if (!Tok.is(Token::KW_if))
+        goto _error3;
+
+    advance();
+
+    E = parseExpression();
+    if(E)
+        expressions.push_back(E);
+    else
+        goto _error3;
+
+    if (!Tok.is(Token::colon))
+        goto _error3;
+
+    advance();
+
+    if (!Tok.is(Token::KW_begin))
+        goto _error3;
+
+    advance();
+
+    while (!Tok.is(Token::KW_end))
+    {
+        if (!Tok.is(Token::ident))
+        {
+            error();
+            goto _error3;
+        }
+
+        else {
+            A = parseAssign();
+            if(A)
+                temp_assignments.push_back(A);
+            else
+                goto _error3;
+        }
+    }
+
+    assignments.push_back(temp_assignments);
+
+    if (!Tok.is(Token::KW_end))
+    {
+        error();
+        goto _error3;
+    }
+
+    advance();
+    
+    while (Tok.is(Token::KW_elif)) {
+        advance();
+
+        E = parseExpression();
+        if(E)
+            expressions.push_back(E);
+        else
+            goto _error3;
+
+        if (!Tok.is(Token::colon))
+            goto _error3;
+
+        advance();
+
+        if (!Tok.is(Token::KW_begin))
+            goto _error3;
+
+        advance();
+
+        temp_assignments.clear();
+        
+        while (!Tok.is(Token::KW_end))
+        {
+            if (!Tok.is(Token::ident))
+            {
+                error();
+                goto _error3;
+            }
+
+            else {
+                A = parseAssign();
+                if(A)
+                    temp_assignments.push_back(A);
+                else
+                    goto _error3;
+            }
+        }
+
+        assignments.push_back(temp_assignments)
+
+        if (!Tok.is(Token::KW_end))
+        {
+            error();
+            goto _error3;
+        }
+
+        advance();
+    }
+    
+    if (Tok.is(Token::KW_else)) {
+        advance();
+        hasElse = true;//NEW
+
+        if (!Tok.is(Token::colon))
+            goto _error3;
+
+        advance();
+
+        if (!Tok.is(Token::KW_begin))
+            goto _error3;
+
+        advance();
+        
+        temp_assignments.clear();
+
+        while (!Tok.is(Token::KW_end))
+        {
+            if (!Tok.is(Token::ident))
+            {
+                error();
+                goto _error3;
+            }
+
+            else {
+                A = parseAssign();
+                if(A)
+                    temp_assignments.push_back(A);
+                else
+                    goto _error3;
+            }
+        }
+
+        assignments.push_back(temp_assignments)
+
+        if (!Tok.is(Token::KW_end))
+        {
+            error();
+            goto _error3;
+        }
+    }
+    return new IfElse(expressions, assignments, hasElse);// NEW
+_error3:
+    while (Tok.getKind() != Token::eoi)
+        advance();
+    return nullptr;
+}
+
+Expr *Parser::parseLoop()
+{
+    Expr *E;
+    Assignment *A;
+    llvm::SmallVector<Assignment *> assignments;
+
+    if (!Tok.is(Token::KW_loopc))
+        goto _error4;
+
+    advance();
+
+    E = parseExpression();
+
+    if (!Tok.is(Token::colon))
+        goto _error4;
+
+    advance();
+
+    if (!Tok.is(Token::KW_begin))
+        goto _error4;
+
+    advance();
+
+    while (!Tok.is(Token::KW_end))
+    {
+        if (!Tok.is(Token::ident))
+        {
+            error();
+            goto _error4;
+        }
+
+        else {
+            A = parseAssign();
+            if(A)
+                assignments.push_back(A);
+            else
+                goto _error4;
+        }
+    }
+
+    if (!Tok.is(Token::KW_end))
+    {
+        error();
+        goto _error4;
+    }
+
+    return new Loop(E, assignments);
+_error4:
+    while (Tok.getKind() != Token::eoi)
+        advance();
+    return nullptr;
+}
+
+Expr *Parser::parseExpression()
+{
+    Expr *Left = parseDisjunction();
+
+    while (Tok.is(Token::KW_logical_or))
+    {
+        BinaryOp::Operator Op = BinaryOp::Or;
+        advance();
+        Expr *Right = parseDisjunction();
+        Left = new BinaryOp(Op, Left, Right);
+    }
+    return Left;
+}
+
+Expr *Parser::parseDisjunction()
+{
+    Expr *Left = parseConjunction();
+
+    while (Tok.is(Token::KW_logical_and))
+    {
+        BinaryOp::Operator Op = BinaryOp::And;
+        advance();
+        Expr *Right = parseConjunction();
+        Left = new BinaryOp(Op, Left, Right);
+    }
+    return Left;
+}
+
+Expr *Parser::parseConjunction()
+{
+    Expr *Left = parseEquality();
+    
+    while (Tok.isOneOf(Token::is_equal, Token::is_not_equal))
+    {
+        BinaryOp::Operator Op =
+            Tok.is(Token::is_equal) ? BinaryOp::IsEq : BinaryOp::IsNEq;
+        advance();
+        Expr *Right = parseEquality();
+        Left = new BinaryOp(Op, Left, Right);
+    }
+    return Left;
+}
+
+Expr *Parser::parseEquality()
+{
+    Expr *Left = parseSoftComparison();
+    
+    while (Tok.isOneOf(Token::soft_comp_greater, Token::soft_comp_lower))
+    {
+        BinaryOp::Operator Op =
+            Tok.is(Token::soft_comp_greater) ? BinaryOp::GrEq : BinaryOp::LoEq;
+        advance();
+        Expr *Right = parseSoftComparison();
+        Left = new BinaryOp(Op, Left, Right);
+    }
+    return Left;
+}
+
+Expr *Parser::parseSoftComparison()
+{
+    Expr *Left = parseHardComparison();
+    
+    while (Tok.isOneOf(Token::hard_comp_greater, Token::hard_comp_lower))
+    {
+        BinaryOp::Operator Op =
+            Tok.is(Token::hard_comp_greater) ? BinaryOp::Gr : BinaryOp::Lo;
+        advance();
+        Expr *Right = parseHardComparison();
+        Left = new BinaryOp(Op, Left, Right);
+    }
+    return Left;
+}
+
+Expr *Parser::parseHardComparison()
+{
+    Expr *Left = parsePlusMinus();
+    
+    while (Tok.isOneOf(Token::plus, Token::minus))
+    {
+        BinaryOp::Operator Op =
+            Tok.is(Token::plus) ? BinaryOp::Plus : BinaryOp::Minus;
+        advance();
+        Expr *Right = parsePlusMinus();
+        Left = new BinaryOp(Op, Left, Right);
+    }
+    return Left;
+}
+
+Expr *Parser::parsePlusMinus()
+{
+    Expr *Left = parseTerm();
+    
+    while (Tok.isOneOf(Token::star, Token::slash, Token::mod))
+    {
+        if (Tok.is(Token::star)) {
+            BinaryOp::Operator Op = BinaryOp::Mul;
+        }
+        else if(Tok.is(Token::slash)) {
+            BinaryOp::Operator Op = BinaryOp::Div;
+        }
+        else {
+            BinaryOp::Operator Op = BinaryOp::Mod;
+        }
+        advance();
+        Expr *Right = parseTerm();
+        Left = new BinaryOp(Op, Left, Right);
+    }
+    return Left;
+}
+
+Expr *Parser::parseTerm()
+{
+    Expr *Left = parseFactor();
+
+    while (Tok.is(Token::power))
+    {
+        BinaryOp::Operator Op = BinaryOp::Pow;
+        advance();
+        Expr *Right = parseFactor();
+        Left = new BinaryOp(Op, Left, Right);
+    }
+    return Left;
+}
+
+Expr *Parser::parseFactor()
+{
+    Expr *Res = nullptr;
+    
+    switch (Tok.getKind())
+    {
+    case Token::number:
+        Res = new Factor(Factor::Number, Tok.getText());
+        advance();
+        break;
+    case Token::ident:
+        Res = new Factor(Factor::Ident, Tok.getText());
+        advance();
+        break;
+    case Token::l_paren:
+        advance();
+        Res = parseExpr();
+        if (!consume(Token::r_paren))
+            break;
+    default: // error handling
+        if (!Res)
+            error();
+        while (!Tok.isOneOf(Token::power, Token::star, Token::slash, 
+                            Token::mod, Token::plus, Token::minus,
+                            Token::hard_comp_greater, Token::hard_comp_lower, 
+                            Token::soft_comp_greater, Token::soft_comp_lower,
+                            Token::is_equal, Token::is_not_equal, Token::KW_logical_and,
+                            Token::KW_logical_or, Token::r_paren, Token::colon,
+                            Token::semicolon, Token::comma, Token::eoi))
+            advance();
+        break;
+    }
+    return Res;
+}
